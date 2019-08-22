2234be3bc3b3f3c95fca12f6bf375f79248b061e
dzh <dzh@gmail.com>
2019-08-22 09:35:17 +0200

    feature-1 create Bundesliga crawler

---
+++ spidz/src/main/kotlin/com/trivago/spiderman/domain/models.kt
@@ -0,0 +1,39 @@
+package com.trivago.spiderman.domain
+
+import com.google.gson.annotations.SerializedName
+
+class DomainEvent {
+    var name = ""
+    @SerializedName("date-time")
+    lateinit var dateTime: DomainDateTime
+    @SerializedName("location")
+    lateinit var location: DomainLocation
+}
+
+class DomainDateTime {
+    var start: String? = null
+    var end: String? = null
+}
+
+class DomainLocation {
+    @SerializedName("adress")
+    lateinit var address: DomainAddress
+
+    var city = ""
+    var country = ""
+    var district = ""
+    var state = ""
+
+    @SerializedName("location")
+    lateinit var geo: DomainGeo
+}
+
+class DomainAddress {
+    var street = ""
+    var number = 0
+}
+
+class DomainGeo {
+    var lat = 0.0
+    var lon = 0.0
+}
---
+++ spidz/src/main/kotlin/com/trivago/spiderman/location/Location.kt
@@ -0,0 +1,8 @@
+package com.trivago.spiderman.location
+
+class Location {
+    var country = ""
+    var city = ""
+    var coordinates = doubleArrayOf(0.0, 0.0)
+    var address = ""
+}
